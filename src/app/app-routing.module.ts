571ba1555d6000de5b8c35f5e152ea11a0110f1f
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-11 19:59:41 -0800

    added pagenotfound to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
