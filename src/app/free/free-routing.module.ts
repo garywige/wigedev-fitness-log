91b0d9382b8d100918ce2766a1274c227c639176
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-12 12:57:55 -0800

    calendar routing

--- src/app/free/free-routing.module.ts
+++ src/app/free/free-routing.module.ts
@@ -1,11 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { NgModule } from '@angular/core';
+import { WorkoutsCalendarComponent } from './workouts-calendar/workouts-calendar.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/free/workouts', pathMatch: 'full'},
-  { path: 'workouts', component: WorkoutsComponent}
+  { path: 'workouts', component: WorkoutsComponent, children: [
+    { path: 'calendar', component: WorkoutsCalendarComponent}
+  ]}
 ];
 
 @NgModule({
