859fe923ff387f260c8f50206792139893e60bf9
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-12 11:28:59 -0800

    added workouts to routing

--- src/app/free/free-routing.module.ts
+++ src/app/free/free-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { WorkoutsComponent } from './workouts/workouts.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/free/workouts', pathMatch: 'full'},
+  { path: 'workouts', component: WorkoutsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
