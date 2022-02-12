32c95c6c31d6668c04da056466f7f760896bb763
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-11 19:58:37 -0800

    added page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { FreeModule } from './free/free.module';
 import { ProModule } from './pro/pro.module';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, FreeModule, ProModule],
   providers: [],
   bootstrap: [AppComponent],
