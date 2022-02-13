4bc60d3cab88b6e2454d36578504f9ec9937b1ba
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-13 12:55:49 -0800

    imported flex layout module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout'
 import { FreeModule } from './free/free.module';
-import { ProModule } from './pro/pro.module';
 import { HomeComponent } from './home/home.component';
+import { MaterialModule } from './material/material.module';
+import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProModule } from './pro/pro.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent, SignInComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule, FreeModule, ProModule, BrowserAnimationsModule, MaterialModule],
+  imports: [BrowserModule, AppRoutingModule, FreeModule, ProModule, BrowserAnimationsModule,
+    MaterialModule, FlexLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
