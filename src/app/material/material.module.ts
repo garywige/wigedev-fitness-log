0fb6b188f986ea425a14a1ef22f6fcc783b8d169
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-13 12:54:01 -0800

    added some material modules to new material feature module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,12 +1,18 @@
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
+const modules = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule
+]
 
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: modules,
+  exports: modules
 })
 export class MaterialModule { }
