312a0038e325b14d66f1c6593bce32fd941f3bdf
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-12 19:37:15 -0800

    added logic for generating selected month

--- src/app/free/workouts-calendar/workouts-calendar.component.ts
+++ src/app/free/workouts-calendar/workouts-calendar.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+class Month {
+  name: string
+  days: number
+
+  constructor(name: string, days: number){
+    this.name = name
+    this.days = days
+  }
+}
 @Component({
   selector: 'app-workouts-calendar',
   templateUrl: './workouts-calendar.component.html',
@@ -8,12 +17,57 @@ import { Component, OnInit } from '@angular/core';
 export class WorkoutsCalendarComponent implements OnInit {
 
   title: string = ''
+  today: Date = new Date()
+  selectedMonth: number = 0
+  months: Array<Month>
+  weeks: Array<Array<number>>
 
-  constructor() { }
+  constructor() {
+    this.months = [
+      new Month('January', 31),
+      new Month('February', (new Date()).getFullYear() % 4 === 0 ? 29 : 28),
+      new Month('March', 31),
+      new Month('April', 30),
+      new Month('May', 31),
+      new Month('June', 30),
+      new Month('July', 31),
+      new Month('August', 31),
+      new Month('September', 30),
+      new Month('October', 31),
+      new Month('November', 30),
+      new Month('December', 31)
+    ]
+
+    this.weeks = []
+  }
 
   ngOnInit(): void {
-    const today = new Date()
-    this.title = `${today.toLocaleString('default', {month: 'long'})} ${today.getFullYear()}`
+    this.title = `${this.today.toLocaleString('default', {month: 'long'})} ${this.today.getFullYear()}`
+
+    // set current month as selected month
+    this.selectedMonth = this.today.getMonth()
+
+    // determine what day the 1st falls on
+    let first = new Date(this.today.getFullYear(), this.today.getMonth(), 1)
+
+    // generate array of weeks that the calendar can use
+    this.weeks.push(new Array(7))
+    let day = 1
+    for(let i = first.getDay(); i < 7; i++){
+      this.weeks[0][i] = day++
+    }
+
+    while(day <= this.months[this.selectedMonth].days){
+
+      this.weeks.push(new Array(7))
+
+      for(let i = 0; i < 7; i++){
+        this.weeks[this.weeks.length - 1][i] = day++
+        if(day > this.months[this.selectedMonth].days){
+          break
+        }
+      }
+    }
   }
 
 }
