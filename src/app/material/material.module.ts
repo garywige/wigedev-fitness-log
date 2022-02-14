5fbc19403aaf3b5ba3b94a6e3495a65ecdacd51a
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-13 19:26:41 -0800

    sign up radio buttons for mat radio

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
+import { MatRadioModule } from '@angular/material/radio'
 import { MatSelectModule } from '@angular/material/select'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { NgModule } from '@angular/core';
@@ -12,7 +13,8 @@ const modules = [
   MatIconModule,
   MatCardModule,
   MatInputModule,
-  MatSelectModule
+  MatSelectModule,
+  MatRadioModule
 ]
 
 
