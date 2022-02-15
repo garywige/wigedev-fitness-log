56236892b823c38c1ff27d13e3e0fede6d894907
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-14 17:39:18 -0800

    edit workout added date picker

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatDialogModule } from '@angular/material/dialog'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
 import { MatMenuModule } from '@angular/material/menu'
+import { MatNativeDateModule } from '@angular/material/core'
 import { MatRadioModule } from '@angular/material/radio'
 import { MatSelectModule } from '@angular/material/select'
 import { MatToolbarModule } from '@angular/material/toolbar'
@@ -20,7 +21,8 @@ const modules = [
   MatRadioModule,
   MatMenuModule,
   MatDialogModule,
-  MatDatepickerModule
+  MatDatepickerModule,
+  MatNativeDateModule
 ]
 
 
