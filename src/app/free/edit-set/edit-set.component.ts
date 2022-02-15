d6cd1681406dcdaa9cdd7092b6c6170bdd4e8387
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-14 20:44:35 -0800

    edit set saved form input to output variable

--- src/app/free/edit-set/edit-set.component.ts
+++ src/app/free/edit-set/edit-set.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'
+
+import { Component } from '@angular/core';
+
+interface set {
+  exercise: string,
+  weight: number,
+  unit: string,
+  reps: number
+}
 
 @Component({
   selector: 'app-edit-set',
@@ -6,7 +15,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-set.component.css']
 })
 export class EditSetComponent {
+  form = new FormGroup({
+    exercise: new FormControl(''),
+    weight: new FormControl(''),
+    unit: new FormControl(''),
+    reps: new FormControl('')
+  })
 
+  output: set = <set>{}
   constructor() { }
+
+  onSubmit(){
+    this.output.exercise = this.form.get('exercise')?.value
+    this.output.weight = +this.form.get('weight')?.value
+    this.output.unit = this.form.get('unit')?.value
+    this.output.reps = +this.form.get('reps')?.value
+  }
 
 }
