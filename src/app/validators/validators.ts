294616a8a7f2a5cafe5cabf3b057570dfef76807
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-15 06:53:08 -0800

    implemented some validators

---
+++ src/app/validators/validators.ts
@@ -0,0 +1,15 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
+
+export function ValidateRegExp(regexp: RegExp): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    return regexp.test(control.value) ? null : {invalid: {value: control.value}}
+  }
+}
+
+export function ValidateInt(): ValidatorFn {
+  return ValidateRegExp(/^\d+$/)
+}
+
+export function ValidateWeight(): ValidatorFn {
+  return ValidateRegExp(/^\d{1,4}(\.\d)?$/)
+}
