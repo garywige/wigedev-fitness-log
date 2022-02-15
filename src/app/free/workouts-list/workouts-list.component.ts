e767edd009c79325d37d4720d91980e326d1be19
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-14 17:12:53 -0800

    linked dialog to add button on workout list

--- src/app/free/workouts-list/workouts-list.component.ts
+++ src/app/free/workouts-list/workouts-list.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EditWorkoutComponent } from '../edit-workout/edit-workout.component';
+import { MatDialog } from '@angular/material/dialog';
+
 @Component({
   selector: 'app-workouts-list',
   templateUrl: './workouts-list.component.html',
   styleUrls: ['./workouts-list.component.css'],
 })
 export class WorkoutsListComponent {
-  constructor() {}
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(){
+    this.dialog.open(EditWorkoutComponent)
+  }
 }
