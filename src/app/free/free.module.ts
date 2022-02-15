ddcd6b5d8c524bd346f1e555703cc3988076df81
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-14 20:15:32 -0800

    free module imported reactive forms module

--- src/app/free/free.module.ts
+++ src/app/free/free.module.ts
@@ -1,16 +1,17 @@
 import { CommonModule } from '@angular/common';
+import { DeleteWorkoutComponent } from './delete-workout/delete-workout.component';
+import { EditSetComponent } from './edit-set/edit-set.component';
+import { EditWorkoutComponent } from './edit-workout/edit-workout.component';
 import { FreeRoutingModule } from './free-routing.module';
 import { MaterialModule } from '../material/material.module';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { WorkoutsCalendarComponent } from './workouts-calendar/workouts-calendar.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
 import { WorkoutsListComponent } from './workouts-list/workouts-list.component';
-import { EditWorkoutComponent } from './edit-workout/edit-workout.component';
-import { EditSetComponent } from './edit-set/edit-set.component';
-import { DeleteWorkoutComponent } from './delete-workout/delete-workout.component';
 
 @NgModule({
   declarations: [WorkoutsComponent, WorkoutsCalendarComponent, WorkoutsListComponent, EditWorkoutComponent, EditSetComponent, DeleteWorkoutComponent],
-  imports: [CommonModule, FreeRoutingModule, MaterialModule],
+  imports: [CommonModule, FreeRoutingModule, MaterialModule, ReactiveFormsModule],
 })
 export class FreeModule {}
