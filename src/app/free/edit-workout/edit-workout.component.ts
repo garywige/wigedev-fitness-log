c02e8c4c53dd364b56898667787cb61ab816535b
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-15 10:18:20 -0800

    edit workout added sets array

--- src/app/free/edit-workout/edit-workout.component.ts
+++ src/app/free/edit-workout/edit-workout.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { DeleteWorkoutComponent } from '../delete-workout/delete-workout.component';
 import { EditSetComponent } from '../edit-set/edit-set.component';
 import { MatDialog } from '@angular/material/dialog';
+import { Set } from '../set';
 
 @Component({
   selector: 'app-edit-workout',
@@ -11,12 +12,15 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class EditWorkoutComponent {
 
+  sets: Set[] = []
+
   constructor(public dialog: MatDialog) { }
 
   openSetDialog(){
     let dialogRef = this.dialog.open(EditSetComponent)
     dialogRef.afterClosed().subscribe(result => {
-      console.log(result)
+      this.sets.push(result)
+      console.log(JSON.stringify(this.sets))
     })
   }
 
