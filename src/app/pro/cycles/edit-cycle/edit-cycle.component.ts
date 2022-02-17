ff0718ad9fe7c76c55132b19c045ec268b47ccc6
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-16 20:24:45 -0800

    edit cycle more markup and form editing

--- src/app/pro/cycles/edit-cycle/edit-cycle.component.ts
+++ src/app/pro/cycles/edit-cycle/edit-cycle.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-edit-cycle',
   templateUrl: './edit-cycle.component.html',
   styleUrls: ['./edit-cycle.component.css']
 })
-export class EditCycleComponent implements OnInit {
+export class EditCycleComponent {
+
+  form: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.pattern(/^(([a-zA-Z0-9]+)\s?)+$/)])
+  })
 
   constructor() { }
 
-  ngOnInit(): void {
+  onSubmit(){
+    let output = {
+      name: this.form.get('name')?.value
+    }
+
+    console.log(output)
   }
 
+  openDeleteDialog(){
+
+  }
 }
--- src/app/pro/pro.module.ts
+++ src/app/pro/pro.module.ts
@@ -4,9 +4,10 @@ import { MaterialModule } from '../material/material.module';
 import { NgModule } from '@angular/core';
 import { ProRoutingModule } from './pro-routing.module';
 import { EditCycleComponent } from './cycles/edit-cycle/edit-cycle.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [CyclesComponent, EditCycleComponent],
-  imports: [CommonModule, ProRoutingModule, MaterialModule],
+  imports: [CommonModule, ProRoutingModule, MaterialModule, ReactiveFormsModule],
 })
 export class ProModule {}
