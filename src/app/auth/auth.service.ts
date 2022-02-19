a6ea193573a2da842ad6898c56dfdeb6c5ae379c
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-19 13:52:43 -0800

    IAuthService interface

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Role } from './auth.enum';
+import { IUser } from '../user/user/user';
+import { Observable } from 'rxjs';
 
 export interface IAuthStatus{
   isAuthenticated: boolean
@@ -15,6 +18,14 @@ export const defaultAuthStatus: IAuthStatus = {
   isAuthenticated: false,
   userRole: Role.None,
   userId: ''
+}
+
+export interface IAuthService {
+  readonly authStatus$: BehaviorSubject<IAuthStatus>
+  readonly currentUser$: BehaviorSubject<IUser>
+  login(email: string, password: string): Observable<void>
+  logout(clearToken?: boolean): void
+  getToken(): string
 }
 
 @Injectable()
