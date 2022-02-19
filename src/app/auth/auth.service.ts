24aa859fac0d6bc7448bf5fcb553427dcc21ad5f
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-02-19 13:47:38 -0800

    auth service created common interfaces

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Role } from './auth.enum';
+
+export interface IAuthStatus{
+  isAuthenticated: boolean
+  userRole: Role
+  userId: string
+}
+
+export interface IServerAuthResponse {
+  accessToken: string
+}
+
+export const defaultAuthStatus: IAuthStatus = {
+  isAuthenticated: false,
+  userRole: Role.None,
+  userId: ''
+}
 
 @Injectable()
 export class AuthService {
