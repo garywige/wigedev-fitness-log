0aeb508b2a1c67e5c0272eda45f33bd5dee514a1
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-03 11:36:16 -0800

    workouts populate cycles array

--- src/app/free/workouts/workouts.component.ts
+++ src/app/free/workouts/workouts.component.ts
@@ -1,10 +1,32 @@
 import { Component, OnInit } from '@angular/core'
+import { map, tap } from 'rxjs'
+import { ApiService, readCyclesElement } from 'src/app/common/services/api/api.service'
+import { UiService } from 'src/app/common/services/ui/ui.service'
 
 @Component({
   selector: 'app-workouts',
   templateUrl: './workouts.component.html',
   styleUrls: ['./workouts.component.css'],
 })
-export class WorkoutsComponent {
-  constructor() {}
+export class WorkoutsComponent implements OnInit {
+  cycles: readCyclesElement[] = []
+
+  constructor(private api: ApiService, private uiService: UiService) {}
+
+  ngOnInit() {
+    // populate cycles
+    this.api.readCycles().pipe(map(result => {
+      if(result?.message){
+        this.uiService.toast('There was an error reading cycles.')
+        return []
+      }
+      else{
+        return result?.cycles
+      }
+    }), tap(cycles => {
+      this.cycles = cycles
+    })).subscribe()
+  }
 }
+
+
