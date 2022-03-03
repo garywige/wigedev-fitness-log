7cb22728fa892eab07ca5328113a94218968f5a4
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-03 07:25:11 -0800

    signup method

--- src/app/common/services/api/api.service.ts
+++ src/app/common/services/api/api.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  signup(email: string, password: string, accountType: string): Observable<signupOutput>{
+    const url = environment.apiurl + '/v1/signup'
+    const reqBody = {
+      email: email,
+      password: password,
+      accountType: accountType
+    }
+
+    return this.http.post<signupOutput>(url, reqBody)
+  }
+}
+
+export interface signupOutput {
+  email: string
 }
