2e523ccfebd077f1f430eb3af86be07f554f1657
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-03 18:42:50 -0800

    edit workout populate form

--- src/app/free/workouts/edit-workout/edit-workout.component.ts
+++ src/app/free/workouts/edit-workout/edit-workout.component.ts
@@ -6,25 +6,53 @@ import { Set } from './set'
 import { ExerciseGroup } from './exercise-group'
 import { UiService } from 'src/app/common/services/ui/ui.service'
 import { MAT_DIALOG_DATA } from '@angular/material/dialog'
+import { ApiService } from 'src/app/common/services/api/api.service'
+import { WorkoutService } from 'src/app/common/services/workout/workout.service'
+import { filter, tap } from 'rxjs'
 
 @Component({
   selector: 'app-edit-workout',
   templateUrl: './edit-workout.component.html',
   styleUrls: ['./edit-workout.component.css'],
 })
-export class EditWorkoutComponent {
+export class EditWorkoutComponent implements OnInit {
   date: Date = new Date()
   groups: ExerciseGroup[] = []
   output: { date: Date; sets: Set[] } | null = null
   isEditMode: boolean = false
 
-  constructor(private uiService: UiService, @Inject(MAT_DIALOG_DATA)data: any) {
+  constructor(private uiService: UiService, @Inject(MAT_DIALOG_DATA)data: any, private api: ApiService, private workoutService: WorkoutService) {
     if(data?.date){
       // adjust date to compensate for timezone
       this.date = data.date
       this.date.setUTCHours(this.date.getTimezoneOffset() / 60)
       this.isEditMode = true
     }
+  }
+
+  ngOnInit() {
+      if(this.isEditMode){
+        // load the workout into the form
+        this.workoutService.selectedCycleId$.pipe(
+          filter(cycleId => cycleId?.length > 0),
+          tap(cycleId => {
+            this.api.readWorkout(this.date, cycleId).pipe(
+              filter(output => output?.message ? false : true),
+              tap(workout => {
+                workout?.sets?.forEach(set => {
+                  this.addSet({
+                    exercise: set?.exercise,
+                    weight: set?.weight,
+                    unit: set?.unit,
+                    reps: set?.repsPrescribed,
+                    completed: set?.repsPerformed
+                  })
+                })
+              })
+            ).subscribe()
+          })
+        ).subscribe()
+      }
   }
 
   openSetDialog() {
@@ -46,12 +74,12 @@ export class EditWorkoutComponent {
   }
 
   addSet(set: Set) {
-    if (this.groups.filter((group) => group.name === set.exercise).length === 0) {
+    if (this.groups.filter((group) => group.name === set.exercise?.name).length === 0) {
       // add new group
-      this.groups.push(new ExerciseGroup(set.exercise, [set]))
+      this.groups.push(new ExerciseGroup(set.exercise?.name, [set]))
     } else {
       // add to existing group
-      this.groups.filter((group) => group.name === set.exercise)[0]?.sets?.push(set)
+      this.groups.filter((group) => group.name === set.exercise?.name)[0]?.sets?.push(set)
     }
   }
 
