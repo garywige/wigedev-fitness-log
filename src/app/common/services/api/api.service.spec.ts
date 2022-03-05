0ce7924cc95f9baad2a2c90aedeb11a744c13ced
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-04 16:57:08 -0800

    api service tests

--- src/app/common/services/api/api.service.spec.ts
+++ src/app/common/services/api/api.service.spec.ts
@@ -45,4 +45,164 @@ describe('ApiService', () => {
       expect(service['http'].post).toHaveBeenCalled()
     })
   })
+
+  describe('readCycles()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readCycles()
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('createCycle()', () => {
+    it('should call post()', () => {
+      // Act
+      service.createCycle('test')
+
+      // Assert
+      expect(service['http'].post).toHaveBeenCalled()
+    })
+  })
+
+  describe('readCycle()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readCycle('test')
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('updateCycle()', () => {
+    it('should call put()', () => {
+      // Act
+      service.updateCycle('test', 'test')
+
+      // Assert
+      expect(service['http'].put).toHaveBeenCalled()
+    })
+  })
+
+  describe('deleteCycle()', () => {
+    it('should call delete()', () => {
+      // Act
+      service.deleteCycle('test')
+
+      // Assert
+      expect(service['http'].delete).toHaveBeenCalled()
+    })
+  })
+
+  describe('readExercises()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readExercises()
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('createExercise()', () => {
+    it('should call post()', () => {
+      // Act
+      service.createCycle('test')
+
+      // Assert
+      expect(service['http'].post).toHaveBeenCalled()
+    })
+  })
+
+  describe('readExercise()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readExercise('test')
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('updateExercise()', () => {
+    it('should call put()', () => {
+      // Act
+      service.updateExercise('test', 'test')
+
+      // Assert
+      expect(service['http'].put).toHaveBeenCalled()
+    })
+  })
+
+  describe('deleteExercise()', () => {
+    it('should call delete()', () => {
+      // Act
+      service.deleteExercise('test')
+
+      // Assert
+      expect(service['http'].delete).toHaveBeenCalled()
+    })
+  })
+
+  describe('readWorkouts()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readWorkouts('test')
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('createWorkout()', () => {
+    it('should call post()', () => {
+      // Act
+      service.createWorkout(new Date(), 'test', [])
+
+      // Assert
+      expect(service['http'].post).toHaveBeenCalled()
+    })
+  })
+
+  describe('readWorkout()', () => {
+    it('should call get()', () => {
+      // Act
+      service.readWorkout(new Date(), 'test')
+
+      // Assert
+      expect(service['http'].get).toHaveBeenCalled()
+    })
+  })
+
+  describe('updateWorkout()', () => {
+    it('should call put()', () => {
+      // Act
+      service.updateWorkout(new Date(), 'test', [])
+
+      // Assert
+      expect(service['http'].put).toHaveBeenCalled()
+    })
+  })
+
+  describe('deleteWorkout()', () => {
+    it('should call delete()', () => {
+      // Act
+      service.deleteWorkout(new Date(), 'test')
+
+      // Assert
+      expect(service['http'].delete).toHaveBeenCalled()
+    })
+  })
+
+  describe('toDateString()', () => {
+    it('should output date in format of YYYY-MM-DD', () => {
+      // Act
+      const result = service['toDateString'](new Date())
+
+      // Assert
+      expect(/^\d{4}-\d{2}-\d{2}$/.test(result)).toEqual(true)
+    })
+  })
 })
