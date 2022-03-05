0c436693fa0776200d3856ffda8810111f445ce6
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-04 20:34:48 -0800

    UI Service tests

--- src/app/common/services/ui/ui.service.spec.ts
+++ src/app/common/services/ui/ui.service.spec.ts
@@ -16,4 +16,29 @@ describe('UiService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy()
   })
+
+  describe('toast()', () => {
+    it('should call snackbar.open()', () => {
+      service['snackbar'].open = jasmine.createSpy<any>()
+      service.toast('test')
+      expect(service['snackbar'].open).toHaveBeenCalled()
+    })
+  })
+
+  describe('showDialog()', () => {
+    it('should call dialog.open()', () => {
+      service['dialog'].open = jasmine.createSpy<any>()
+      const component = jasmine.createSpyObj<any>('ComponentType', [''])
+      service.showDialog(component)
+      expect(service['dialog'].open).toHaveBeenCalled()
+    })
+  })
+
+  describe('closeAllDialogs()', () => {
+    it('should call dialog.closeAll()', () => {
+      service['dialog'].closeAll = jasmine.createSpy<any>()
+      service.closeAllDialogs()
+      expect(service['dialog'].closeAll).toHaveBeenCalled()
+    })
+  })
 })
