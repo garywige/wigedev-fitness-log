176fae25ba400521beaf02c3d1699d32fdae77bd
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-14 06:15:58 -0700

    modified free routing

--- src/app/free/free-routing.module.ts
+++ src/app/free/free-routing.module.ts
@@ -1,20 +1,17 @@
 import { RouterModule, Routes } from '@angular/router'
 
-import { NgModule } from '@angular/core'
-import { WorkoutsCalendarComponent } from './workouts/workouts-calendar/workouts-calendar.component'
-import { WorkoutsComponent } from './workouts/workouts.component'
-import { WorkoutsListComponent } from './workouts/workouts-list/workouts-list.component'
 import { AuthGuardService } from '../common/services/auth/auth-guard.service'
+import { NgModule } from '@angular/core'
+import { Role } from '../common/services/auth/auth.enum'
+import { WorkoutsComponent } from './workouts/workouts.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/free/workouts/calendar', pathMatch: 'full' },
+  { path: '', redirectTo: '/free/workouts', pathMatch: 'full' },
   {
     path: 'workouts',
     component: WorkoutsComponent,
-    children: [
-      { path: 'calendar', component: WorkoutsCalendarComponent, canActivate: [AuthGuardService] },
-      { path: 'list', component: WorkoutsListComponent, canActivate: [AuthGuardService] },
-    ],
+    canActivate: [AuthGuardService],
+    data: { expectedRole: Role.Free}
   },
 ]
 
