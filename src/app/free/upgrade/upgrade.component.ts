c2269fc75429e45744e2b5001e2ec4c7248c92d5
Gary Wige <91767400+garywige@users.noreply.github.com>
2022-03-22 20:27:51 -0700

    built payment form

--- src/app/free/upgrade/upgrade.component.ts
+++ src/app/free/upgrade/upgrade.component.ts
@@ -1,3 +1,4 @@
+import { Card, Payments, Square } from '@square/web-payments-sdk-types'
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpgradeComponent implements OnInit {
 
+  private appId = 'sandbox-sq0idb-Cev7uB4UAAPhOfL4ZWRKMQ'
+  private locationId = 'LNXH23P4CY14T'
+
   constructor() { }
 
   ngOnInit(): void {
+    if(!window.Square){
+      throw new Error('Square.js failed to load properly')
+    }
+
+    const payments = window.Square.payments(this.appId, this.locationId)
+    let card: Card
+
+    this.initializeCard(payments).then(output => {
+      card = output
+
+      // TODO: Create card payment
+    }).catch(e => console.error('Initializing card failed', e))
   }
 
+  async initializeCard(payments: Payments): Promise<Card>{
+    const card = await payments.card()
+    await card.attach('#card-container')
+    return card
+  }
 }
